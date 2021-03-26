f26315d06b640724c9c2b3f8b917a75946f4201f
Tenzin Dhondup <txd5857@g.rit.edu>
2021-03-26 16:54:40 -0400

    new

--- srdev/client/routes/veterans.js
+++ srdev/client/routes/veterans.js
@@ -2,11 +2,13 @@ const express = require('express');
 const veteranRouter = express.Router();
 const axios = require('axios');
 
+const veteran_list = [];
 veteranRouter.get('/veterans', async(req,res)=>{
 
     try {
          const veteranAPI = await axios.get('http://localhost:5002/api/veterans');
          console.log(veteranAPI.data);
+         veteran_list = veteranAPI.data;
          res.render('veterans', { veterans : veteranAPI.data});
     }catch (error) {
         if(error.response){
@@ -17,16 +19,22 @@ veteranRouter.get('/veterans', async(req,res)=>{
     // res.render('veterans');
 });
 
-veteranRouter.get('/veterans/:id', (req,res)=>{
-    res.render('veteran', {
-        id: req.params.id
-    });
+veteranRouter.get('/:id', async(req,res)=>{
+    const vetid = req.params.id;
+    const url = "http://localhost:5002/api/veterans/"+vetid;
+
+    console.log(url);
+    try {
+        const veteranAPI = await axios.get('/api/veterans/3700');
+        console.log(veteranAPI.data);
+        res.render('veteran', { veteran : veteranAPI.data});
+   }catch (error) {
+       if(error.response){
+           console.log(error.response.data);
+       }
+   }
 });
 
- 
-veteranRouter.get('/homepage', (req,res)=>{
-    res.render('homepage');
-});
 
 
 module.exports = veteranRouter;
