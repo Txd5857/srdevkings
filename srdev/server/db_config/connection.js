7e2e5c4556362a44bdad6d35989175a5ccb1f5d0
Amina Mahmood <37802650+axm6392@users.noreply.github.com>
2021-03-30 01:44:56 -0400

    Added veterans styles
    Added veterans styles

--- srdev/server/db_config/connection.js
+++ srdev/server/db_config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 var mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "students",
     database: "honor_flight",
     multipleStatements: true
 });
